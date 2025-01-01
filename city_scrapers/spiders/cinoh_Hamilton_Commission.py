5b22ae5a59b44c61eacb9e7a06429c3d2027bf2d
vsspnkkvr <112337917+vsspnkkvr@users.noreply.github.com>
2025-01-01 04:11:25 -0500

    cinoh_Hamilton_Commission working build 1

---
+++ city_scrapers/spiders/cinoh_Hamilton_Commission.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+from city_scrapers_core.constants import COMMISSION
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.parser import parse
+from urllib.parse import urljoin
+
+
+class CinohHamiltonCommissionSpider(CityScrapersSpider):
+    name = "cinoh_Hamilton_Commission"
+    agency = "Hamilton County Board of Commissioners"
+    timezone = "America/Chicago"
+    start_urls = ["https://hcjfsonbase.jfs.hamilton-co.org/OnBaseAgendaOnline"]
+
+    def parse(self, response):
+        """
+        Parse upcoming and past meetings from the Hamilton County Board of Commissioners meetings table.
+        """
+        location = {
+            "name": "Todd B. Portune Center for County Government",
+            "address": "138 East Court Street, Room 603, Cincinnati, OH 45202",
+        }
+
+        for item in response.css(".meeting-row"):
+            meeting = Meeting(
+                title=self._parse_title(item),
+                description="",
+                classification=COMMISSION,
+                start=self._parse_start(item),
+                end=None,
+                all_day=False,
+                time_notes="",
+                location=location,
+                links=self._parse_links(item),
+                source="https://hcjfsonbase.jfs.hamilton-co.org/OnBaseAgendaOnline",
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse meeting title."""
+        string = item.css(".visible-xs::text").get()
+        return string.strip()
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        date = item.css("td::text")[2].get()
+        return parse(date, fuzzy=True)
+
+    # all three link types on the table--Agenda, Minutes, and View Media--all link to the same page
+    def _parse_links(self, item):
+        """Parse links."""
+        url_first_half = "https://hcjfsonbase.jfs.hamilton-co.org"
+        url_second_half = item.css("::attr(href)").get()
+        url = urljoin(url_first_half, url_second_half)
+        if url == "https://hcjfsonbase.jfs.hamilton-co.org":
+            return [{"title": "No links included", "href": "N/A"}]
+        else:
+            return [{"title": "Agenda, Notes, and Media", "href": url}]
+       
---
+++ tests/test_cinoh_Hamilton_Commission.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import COMMISSION
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.cinoh_Hamilton_Commission import CinohHamiltonCommissionSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "cinoh_Hamilton_Commission.html"),
+    url="https://hcjfsonbase.jfs.hamilton-co.org/OnBaseAgendaOnline",
+)
+spider = CinohHamiltonCommissionSpider()
+
+freezer = freeze_time("2024-12-31")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_title():
+     assert parsed_items[0]["title"] == "Commissioners Organizational Meeting"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2025, 1, 7, 10, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] == None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "cinoh_Hamilton_Commission/202501071000/x/commissioners_organizational_meeting"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "tentative"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+            "name": "Todd B. Portune Center for County Government",
+            "address": "138 East Court Street, Room 603, Cincinnati, OH 45202",
+        }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://hcjfsonbase.jfs.hamilton-co.org/OnBaseAgendaOnline"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [{
+        "href": "N/A",
+        "title": "No links included"
+    }]
+    assert parsed_items[20]["links"] == [{
+        "href": "https://hcjfsonbase.jfs.hamilton-co.org/OnBaseAgendaOnline/Meetings/ViewMeeting?id=2597&doctype=1",
+        "title": "Agenda, Notes, and Media"
+    }]
+    
+
+def test_classification():
+    assert parsed_items[0]["classification"] == COMMISSION
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
